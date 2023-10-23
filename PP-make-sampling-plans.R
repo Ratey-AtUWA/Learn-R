ee293d879e85c8c1cab6df3b73c79fa8a2d7ebd6
Andrew Rate <andrew.rate@uwa.edu.au>
2023-10-23 12:07:13 +0800

    make links to github raw files absolute

--- PP-make-sampling-plans.R
+++ PP-make-sampling-plans.R
@@ -8,19 +8,28 @@ library(maptiles)
 library(prettymapr)
 library(TeachingDemos)
 
-pp_samparea <- st_read("sampling_area.kml") |> st_transform(crs=st_crs(32750))
+pp_samparea <- 
+  st_read("https://github.com/Ratey-AtUWA/Learn-R/raw/main/PP/sampling_area.kml") |> 
+  st_transform(crs=st_crs(32750))
 
 sa1 <- as.matrix(st_coordinates(pp_samparea)[,1:2])
-sah1 <- st_read("rushes.kml") |> st_transform(crs=st_crs(32750)) |> 
+sah1 <- 
+  st_read("https://github.com/Ratey-AtUWA/Learn-R/raw/main/PP/rushes.kml") |> 
+  st_transform(crs=st_crs(32750)) |> 
   st_coordinates() |> as.matrix()
-sah2 <- st_read("dense-veg.kml") |> st_transform(crs=st_crs(32750)) |> 
+sah2 <- 
+  st_read("https://github.com/Ratey-AtUWA/Learn-R/raw/main/PP/dense-veg.kml") |> 
+  st_transform(crs=st_crs(32750)) |> 
   st_coordinates() |> as.matrix()
-sah3 <- st_read("depot.kml") |> st_transform(crs=st_crs(32750)) |> 
+sah3 <- 
+  st_read("https://github.com/Ratey-AtUWA/Learn-R/raw/main/PP/depot.kml") |> 
+  st_transform(crs=st_crs(32750)) |> 
   st_coordinates() |> as.matrix()
 samppoly <- st_polygon(list(sa1,sah1[,1:2],sah2[,1:2],sah3[,1:2])) 
 samppoly <- st_sfc(samppoly, crs=st_crs(32750))
 
-pp0 <- read.csv(file="PelicanPtResBorder.csv")
+pp0 <- 
+  read.csv(file="https://github.com/Ratey-AtUWA/Learn-R/raw/main/PP/PelicanPtResBorder.csv")
 ppb <- st_polygon(list(as.matrix(pp0))) |> st_sfc(crs=st_crs(32750))
 
 ppext <- st_as_sf(data.frame(x= c(388550,389150), y=c(6460300,6460700)),
